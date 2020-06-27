aba03becb74bd801c657bb6cf3c4e489ab617469
dn0912 <nguyen.duc0912@googlemail.com>
2020-06-27 18:51:51 +0200

    add basic dynamodb service

--- budget_control_system/src/index.js
+++ budget_control_system/src/index.js
@@ -1,11 +1,57 @@
 import express from 'express'
+import bodyParser from 'body-parser'
+import superagent from 'superagent'
+import HttpStatus from 'http-status-codes'
+import DynamoDB from './service/trace-store/dynamo'
 
-const app = express()
 const port = process.env.PORT || 3000
+const traceStore = new DynamoDB()
+const app = express()
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.text({
+  type: ['json', 'text']
+}))
 
 app.get('/', (req, res) => res.status(200).json({
   hello: 'world!',
 }))
+
+app.post('/start-tracing', async (req, res) => {
+  // const { jobUrl } = req.body
+  const jobUrl = 'https://www.google.com'
+  // const jobUrl = 'https://dzsq601tu2.execute-api.eu-central-1.amazonaws.com/dev/start-single-job'
+
+  const response = await superagent
+    .get(jobUrl)
+
+  // console.log('+++', response)
+  console.log('+++data', req.body)
+
+  res.status(HttpStatus.OK).json({
+    hello: 'there',
+  })
+})
+
+app.post('/stop', () => {
+
+})
+
+app.post('/test-put-db', async (req, res) => {
+  console.log('+++data', req.body)
+  const createdItem = await traceStore.put('test')
+  console.log('+++createdItem', createdItem)
+  res.status(HttpStatus.CREATED).json({
+    hello: 'world'
+  })
+})
+
+app.get('/test-get-db', async (req, res) => {
+  const createdItem = await traceStore.get()
+  console.log('+++createdItem', createdItem)
+  res.status(HttpStatus.OK).json({
+    hello: 'world'
+  })
+})
 
 app.listen(
   port,
---
+++ budget_control_system/src/service/trace-store/dynamo.js
@@ -0,0 +1,58 @@
+import AWS from 'aws-sdk'
+import uuid from 'node-uuid'
+
+const credentials = new AWS.SharedIniFileCredentials({ profile: process.env.AWS_PROFILE })
+AWS.config.credentials = credentials
+
+console.log('+++process.env', process.env.AWS_RESOURCE_REGION)
+
+AWS.config.update({
+  region: process.env.AWS_RESOURCE_REGION,
+})
+
+const dynamoDocClient = new AWS.DynamoDB.DocumentClient()
+// const update = promisify(dynamoDb.update.bind(dynamoDb))
+
+class DynamoDB {
+  constructor() {
+    this.tableName = 'trace-record'
+    this.ddb = dynamoDocClient
+  }
+
+  async put(traceRecord) {
+    try {
+      const storeItem = {
+        traceId: uuid.v4(),
+        hello: 'world',
+      }
+
+      const docClientParams = {
+        TableName: this.tableName,
+        Item: storeItem,
+        ReturnValues: 'ALL_OLD',
+      }
+      await this.ddb.put(docClientParams).promise()
+      return storeItem
+    } catch (err) {
+      console.log('err', err)
+    }
+  }
+
+  async get(traceId) {
+    try {
+      const param = {
+        TableName: this.tableName,
+        Key: {
+          traceId,
+        },
+      }
+      const dbRecord = await this.ddb.get(param).promise()
+      console.log('+++dbRecord', dbRecord)
+      return dbRecord
+    } catch (err) {
+      console.log('err', err)
+    }
+  }
+}
+
+export default DynamoDB
