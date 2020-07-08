b1ac12d010bf3432e9c6d8dabdcffd44d90ac14b
dn0912 <nguyen.duc0912@googlemail.com>
2020-07-08 11:23:36 +0200

    improve dummy enpoints using params instead of hardcoded values

--- budget_control_system/src/index.js
+++ budget_control_system/src/index.js
@@ -36,7 +36,7 @@ app.get('/', (req, res) => res.status(200).json({
 app.post('/start-tracing', async (req, res) => {
   // const { jobUrl } = req.body
   const dateNowUnix = Date.now()
-  const jobUrl = 'https://dzsq601tu2.execute-api.eu-central-1.amazonaws.com/dev/start-job'
+  const jobUrl = 'https://8horir8nei.execute-api.eu-central-1.amazonaws.com/dev/start-job'
   const jobId = uuid.v4()
   const response = await superagent
     .post(jobUrl)
@@ -88,11 +88,13 @@ app.get('/test-get-db', async (req, res) => {
 })
 
 // AWS Price List Service API
-app.get('/test-get-prices', async (req, res) => {
+// Get product details with AttributeNames
+app.get('/test-get-products', async (req, res) => {
   const response = await priceList.describeServices()
   console.log('+++response', response)
-  console.log('+++Service', response.Services.find((srvc) => srvc.ServiceCode === 'AWSLambda'))
-  console.log('+++Service', response.Services.find((srvc) => srvc.ServiceCode === 'AmazonDynamoDB'))
+  console.log('+++ServiceAWSLambda', response.Services.find((srvc) => srvc.ServiceCode === 'AWSLambda'))
+  console.log('+++ServiceAmazonDynamoDB', response.Services.find((srvc) => srvc.ServiceCode === 'AmazonDynamoDB'))
+  console.log('+++ServiceAWSQueueService', response.Services.find((srvc) => srvc.ServiceCode === 'AWSQueueService'))
 
   console.log('+++describeLambdaServices', (await priceList.describeLambdaServices()).Services[0])
   res.status(HttpStatus.OK).json({
@@ -100,19 +102,42 @@ app.get('/test-get-prices', async (req, res) => {
   })
 })
 
-app.get('/test-get-products', async (req, res) => {
-  // const response = await priceList.getLambdaProducts()
-  // const response = await priceList.getS3Products()
-  // console.log('+++response', serialize(response))
-  console.log('+++describeSQSServices', serialize(await priceList.getSQSProducts()))
+// Get product pricing
+/* Example curl:
+curl http://localhost:8080/test-get-product-prices/lambda
+*/
+app.get('/test-get-product-prices/:service', async (req, res) => {
+  const { service } = req.params
+  let response
+  switch (service) {
+    case 'lambda':
+      response = await priceList.getLambdaProducts()
+      break
+    case 's3':
+      response = await priceList.getS3Products()
+      break
+    case 'sqs':
+      response = await priceList.getSQSProducts()
+      break
+    default:
+      break
+  }
+
+  console.log(`+++response ${service}`, serialize(response))
 
   res.status(HttpStatus.OK).json({
     hello: 'world'
   })
 })
 
-app.get('/test-get-xraydata', async (req, res) => {
-  const traceIds = ['1-5efdc64d-851fcc5c2219f4c03a07f6c8']
+// Get xray data based on traceId
+/* Example curl:
+curl http://localhost:8080/test-get-xraydata/1-5f05866b-d70cfffe81bd667ecb5bab3a,1-5f058594-b735a8ec4f0aa1ee3eea48a0
+*/
+app.get('/test-get-xraydata/:traceIds', async (req, res) => {
+  const traceIds = req.params.traceIds.split(',')
+  console.log('+++traceIds', traceIds)
+  // const traceIds = ['1-5efdc64d-851fcc5c2219f4c03a07f6c8']
   const traceData = await tracer.getXRayTraces(traceIds)
 
   console.log('+++traceData', serialize(traceData))
@@ -139,9 +164,10 @@ app.get('/test-get-xraydata', async (req, res) => {
   })
 })
 
-app.get('/test-get-xraysummary', async (req, res) => {
+app.get('/test-get-xraysummary/:startTime', async (req, res) => {
   // Dateformat in traces are like: 1593786073.729
-  const startTime = 1593794642856 / 1000
+  // const startTime = 1594197396137 / 1000
+  const startTime = parseInt(req.params.startTime, 10) / 1000
   const endTime = Date.now() / 1000
   const traceData = await tracer.getXRayTraceSummaries(startTime, endTime)
   console.log('+++traceData++++', traceData)
