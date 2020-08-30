51ac306fdc83bc9a8bbe96fd8c794677c92dc600
dn0912 <nguyen.duc0912@googlemail.com>
2020-08-30 19:00:28 +0200

    modify ec2 instance setup script and add documentation

--- README.md
+++ README.md
@@ -17,3 +17,73 @@ sls logs -f [FUNCTION NAME] -t --aws-profile [PROFILE]
 # remove services in current working directory
 sls remove --aws-profile [PROFILE]
 ```
+
+
+# Setup
+
+## Setup EC2 instance with Redis
+
+### Create ec2 instance per AWS CLI
+Note:
+- `sg-0cd4e8a87c427827e` is a Security Group with Inbound rules for SSH communication and for Redis: TCP with port 6379 open to the public 0.0.0.0/0
+- `ec2_test_22_07_2020` is a generated key name for SSH connection
+
+```bash
+aws ec2 run-instances \
+  --image-id ami-04932daa2567651e7 \
+  --count 1 \
+  --instance-type t2.micro \
+  --block-device-mappings 'DeviceName=/dev/sda1,Ebs={VolumeSize=30}' \
+  --security-group-ids sg-0cd4e8a87c427827e \
+  --key-name ec2_test_22_07_2020 \
+  --profile duc \
+  --region eu-central-1
+```
+
+### Connect to instance for example with ssh
+```bash
+ssh -i "ec2_test_22_07_2020.pem" root@xxxxxxxxxx.eu-central-1.compute.amazonaws.com
+```
+
+
+### Copy shell script `ec2-isntance-setup.sh` to instance and run it:
+```bash
+vim ec2-isntance-setup.sh
+
+# copy content from `ec2-isntance-setup.sh` to file and save
+
+chmod 755 ec2-isntance-setup.sh
+
+./ec2-isntance-setup.sh # AWS CLI and TCL (for Redis) needs to be confirmed with `Y` on installation
+```
+
+After all installation, Redis server should be running on Port `6379` and job budget application on Port `3000`
+
+### Manually test whether all services are installed
+```bash
+npm --version
+node --version
+aws --version
+redis-cli --version
+redis-server --version
+```
+
+### Manually start Redis server and job budget application
+```bash
+redis-server /home/ubuntu/redis-stable/redis.conf &
+
+cd ~/SBDP_job_budgets/budget_control_system
+
+# Runs on default port is 3000 or set per env var PORT=XXXX
+npm run start
+```
+
+### Find your AWS access key, AWS secret, AWS Default region and Redis password
+
+```
+cat ~/.aws/credentials
+
+cat ~/SBDP_job_budgets/budget_control_system/.env
+
+cat ~/redis-stable/redis.conf | grep "requirepass "
+```
--- ec2-instance-setup.sh
+++ ec2-instance-setup.sh
@@ -1,18 +1,29 @@
 #!/bin/bash
+echo "Please provide the necessary data"
+echo "#################################"
+read -p "AWS access key id:" INPUT_AWS_KEY
+read -sp "AWS secret access key:" INPUT_AWS_SECRET
+echo ""
+read -p "AWS resource region:" INPUT_AWS_RESOURCE_REGION
+read -sp "Redis instance password: " INPUT_REDIS_PASSWORD
+
+# variable decleration
+AWS_KEY=$INPUT_AWS_KEY
+AWS_SECRET=$INPUT_AWS_SECRET
+AWS_RESOURCE_REGION=$INPUT_AWS_RESOURCE_REGION
+REDIS_PASSWORD=$INPUT_REDIS_PASSWORD
+
+REDIS_CONFIG_FILE_PATH=/home/ubuntu/redis-stable/redis.conf
 
 sudo apt-get update
 
-# Install nvm and node
-cd ~
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-. ~/.nvm/nvm.sh
-nvm install node
-node -e "console.log('Running Node.js ' + process.version)"
+# Install node
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+sudo apt install nodejs
+
 
 # Install git
-cd ~
 sudo apt-get install git
-# git --version
 
 # Install aws
 cd ~
@@ -20,9 +31,12 @@ sudo apt-get install -y unzip
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
-# aws --version
+
 mkdir ~/.aws
 touch ~/.aws/credentials
+echo "[default]" >> ~/.aws/credentials
+echo "aws_access_key_id=$AWS_KEY" >> ~/.aws/credentials
+echo "aws_secret_access_key=$AWS_SECRET" >> ~/.aws/credentials
 
 # Install docker
 # sudo apt install docker.io
@@ -42,15 +56,19 @@ wget http://download.redis.io/redis-stable.tar.gz
 tar xvzf redis-stable.tar.gz
 cd ./redis-stable
 sudo make install
-sed -i 's/protected-mode yes/protected-mode no/' /home/ubuntu/redis-stable/redis.conf
-sed -i 's/bind\s127.0.0.1/bind 0.0.0.0/' /home/ubuntu/redis-stable/redis.conf
-sed -i "s/# requirepass.*/requirepass this-is-my-password-0000/" /home/ubuntu/redis-stable/redis.conf
-# redis-server path/to/redis.conf # start redis server with conf file
+sed -i 's/protected-mode yes/protected-mode no/' $REDIS_CONFIG_FILE_PATH
+sed -i 's/bind\s127.0.0.1/bind 0.0.0.0/' $REDIS_CONFIG_FILE_PATH
+sed -i "s/# requirepass.*/requirepass $REDIS_PASSWORD/" $REDIS_CONFIG_FILE_PATH
 # redis-cli -a <password>
 
 # Clone project and run
 cd ~
 git clone https://github.com/dn0912/SBDP_job_budgets.git
-cd SBDP_job_budgets/budget_control_system/
+cd ./SBDP_job_budgets/budget_control_system
 npm i
-# AWS_RESOURCE_REGION=eu-central-1 npm run start
+
+echo "AWS_RESOURCE_REGION=$AWS_RESOURCE_REGION" >> ~/SBDP_job_budgets/budget_control_system/.env
+
+# start services
+redis-server $REDIS_CONFIG_FILE_PATH &
+npm run start
