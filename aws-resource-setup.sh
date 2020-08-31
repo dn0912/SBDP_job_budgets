be9154ba8b21cd6604f5367fa2fc069001ae44e7
dn0912 <nguyen.duc0912@googlemail.com>
2020-09-01 01:06:58 +0200

    add s3 bucket setup and test data upload in script

--- aws-resource-setup.sh
+++ aws-resource-setup.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
+CURRENT_WORKING_DIR=$(pwd)
+echo $CURRENT_WORKING_DIR
+
 echo "Please provide the necessary data"
 echo "#################################"
 read -p "AWS access key id:" INPUT_AWS_KEY
 read -sp "AWS secret access key:" INPUT_AWS_SECRET
 echo ""
-read -p "AWS resource region:" INPUT_AWS_RESOURCE_REGION
+read -p "AWS resource region [eu-central-1]:" INPUT_AWS_RESOURCE_REGION
+INPUT_AWS_RESOURCE_REGION=${INPUT_AWS_RESOURCE_REGION:-eu-central-1}
+echo $INPUT_AWS_RESOURCE_REGION
 read -p "AWS key name for SSH:" INPUT_AWS_KEY_NAME
 
+
+
+CURRENT_WORKING_DIR=$(pwd)
 
 # Security group
 AWS_ACCESS_KEY_ID=$INPUT_AWS_KEY AWS_SECRET_ACCESS_KEY=$INPUT_AWS_SECRET aws ec2 create-security-group \
@@ -18,14 +26,6 @@ AWS_ACCESS_KEY_ID=$INPUT_AWS_KEY AWS_SECRET_ACCESS_KEY=$INPUT_AWS_SECRET aws ec2
 # Save security group id in var
 SECURITY_GROUP_ID="$(AWS_ACCESS_KEY_ID="$INPUT_AWS_KEY" AWS_SECRET_ACCESS_KEY="$INPUT_AWS_SECRET" aws ec2 describe-security-groups --group-names job-budget-security-group --query 'SecurityGroups[*].[GroupId]' --output text --region "$INPUT_AWS_RESOURCE_REGION")"
 
-# Add rule to allow inbound SSH traffic 
-# AWS_ACCESS_KEY_ID=$INPUT_AWS_KEY AWS_SECRET_ACCESS_KEY=$INPUT_AWS_SECRET aws ec2 authorize-security-group-ingress \
-#   --group-id $SECURITY_GROUP_ID \
-#   --protocol tcp \
-#   --port 22 \
-#   --cidr 0.0.0.0/0 \
-#   --region $INPUT_AWS_RESOURCE_REGION
-
 # Add rule to allow inbound traffic - SSH, Redis and Node app
 AWS_ACCESS_KEY_ID=$INPUT_AWS_KEY AWS_SECRET_ACCESS_KEY=$INPUT_AWS_SECRET aws ec2 authorize-security-group-ingress \
     --group-id $SECURITY_GROUP_ID \
@@ -35,6 +35,9 @@ AWS_ACCESS_KEY_ID=$INPUT_AWS_KEY AWS_SECRET_ACCESS_KEY=$INPUT_AWS_SECRET aws ec2
       IpProtocol=tcp,FromPort=3000,ToPort=3000,IpRanges='[{CidrIp=0.0.0.0/0,Description="Node app"}]' \
     --region $INPUT_AWS_RESOURCE_REGION
 
+echo "Sucessfully created security group..."
+
+# Create ec2 instance with security group
 AWS_ACCESS_KEY_ID=$INPUT_AWS_KEY AWS_SECRET_ACCESS_KEY=$INPUT_AWS_SECRET aws ec2 run-instances \
   --image-id ami-04932daa2567651e7 \
   --count 1 \
@@ -43,3 +46,50 @@ AWS_ACCESS_KEY_ID=$INPUT_AWS_KEY AWS_SECRET_ACCESS_KEY=$INPUT_AWS_SECRET aws ec2
   --security-group-ids $SECURITY_GROUP_ID \
   --key-name $INPUT_AWS_KEY_NAME \
   --region $INPUT_AWS_RESOURCE_REGION
+
+echo "Sucessfully created ec instance..."
+
+####################
+# Generate test data
+
+cd "$CURRENT_WORKING_DIR/test_data_generator"
+npm i
+mkdir test_data
+
+echo "Start generating mock data..."
+
+# Generate 4 data files with 500, 1000, 1500 and 2000 mocked tasks
+./node_modules/.bin/babel-node scripts/1-generate-task-data.js -n 500 | json_pp > ./test_data/test_with_description_title_change_500_single.json
+
+./node_modules/.bin/babel-node scripts/1-generate-task-data.js -n 1000 | json_pp > ./test_data/test_with_description_title_change_1000_single.json
+
+./node_modules/.bin/babel-node scripts/1-generate-task-data.js -n 1500 | json_pp > ./test_data/test_with_description_title_change_1500_single.json
+
+./node_modules/.bin/babel-node scripts/1-generate-task-data.js -n 2000 | json_pp > ./test_data/test_with_description_title_change_2000_single.json
+
+echo "Sucessfully generated test data..."
+
+Create S3 bucket
+AWS_ACCESS_KEY_ID=$INPUT_AWS_KEY AWS_SECRET_ACCESS_KEY=$INPUT_AWS_SECRET aws s3api create-bucket \
+  --bucket test-task-update-data-v2 \
+  --create-bucket-configuration LocationConstraint=$INPUT_AWS_RESOURCE_REGION \
+  --region $INPUT_AWS_RESOURCE_REGION
+
+# Create subresult folder in S3 bucket
+AWS_ACCESS_KEY_ID=$INPUT_AWS_KEY AWS_SECRET_ACCESS_KEY=$INPUT_AWS_SECRET aws s3api put-object \
+  --bucket test-task-update-data-v2 \
+  --key test_results/ \
+  --region $INPUT_AWS_RESOURCE_REGION
+
+# Create result folder in S3 bucket
+AWS_ACCESS_KEY_ID=$INPUT_AWS_KEY AWS_SECRET_ACCESS_KEY=$INPUT_AWS_SECRET aws s3api put-object \
+  --bucket test-task-update-data-v2 \
+  --key gsd/ \
+  --region $INPUT_AWS_RESOURCE_REGION
+
+# Upload generated test data to S3 bucket
+AWS_ACCESS_KEY_ID=$INPUT_AWS_KEY AWS_SECRET_ACCESS_KEY=$INPUT_AWS_SECRET aws s3 cp "$CURRENT_WORKING_DIR/test_data_generator/test_data/" s3://test-task-update-data-v2/ \
+  --recursive \
+  --region $INPUT_AWS_RESOURCE_REGION
+
+echo "Sucessfully uploaded test data to S3..."
