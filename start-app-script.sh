ab88cf06d8b5f0c92c766f2b35a5ca194d545d4c
dn0912 <nguyen.duc0912@googlemail.com>
2020-09-15 18:12:02 +0200

    add start app script

---
+++ start-app-script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+CURRENT_WORKING_DIR=$(pwd)
+
+echo "Starting redis server and budget control app"
+
+redis-server /home/ubuntu/redis-stable/redis.conf &
+npm run start --prefix "$CURRENT_WORKING_DIR/budget_control_system"
